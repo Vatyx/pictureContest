1890cb26c1ec63b963f1c61c75575a1ac74c5f17
Sahil Dhanju <sahildhanju1@gmail.com>
2016-07-24 01:49:18 -0700

    tryin

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var User = require('../models/User.js');
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
 
